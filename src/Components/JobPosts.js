94fe53495692b18e8195afd9a4539d815b633094
Sparsh Kansal <sparshkansal93@gmail.com>
2024-03-30 02:48:06 +0530

    filters added and completed

--- src/Components/JobPosts.js
+++ src/Components/JobPosts.js
@@ -1,39 +1,107 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import postContext from '../context/posts/postContext';
 import PostCard from './PostCard';
+import '../App.css';
 
 
 const JobPosts = () => {
     const context = useContext(postContext);
     const { posts } = context;
+    const [selectedFilters, setSelectedFilters] = useState([]);
+    const [filteredPosts, setFilteredPosts] = useState(posts);
+    const rolesSet = new Set(posts.map(post => post.role));
+    const filters = Array.from(rolesSet);
+
+    const [clickedFilters, setClickedFilters] = useState({});
+
+    const handleClick = (selectedCategory) => {
+        setClickedFilters(prevState => ({
+            ...prevState,
+            [selectedCategory]: !prevState[selectedCategory]
+        }));
+
+        if (selectedFilters.includes(selectedCategory)) {
+            let filters = selectedFilters.filter((el) => el !== selectedCategory);
+            setSelectedFilters(filters);
+        } else {
+            setSelectedFilters([...selectedFilters, selectedCategory]);
+        }
+    };
+
+    useEffect(() => {
+        const filterItems = () => {
+            if (selectedFilters.length > 0) {
+                const filtered = posts.filter(post => {
+                    return selectedFilters.includes(post.role);
+                });
+                setFilteredPosts(filtered);
+            } else {
+                setFilteredPosts([...posts]);
+            }
+        };
+        filterItems();
+    }, [selectedFilters, posts]);
+    
+
+
     return (
         <div style={{ display: 'flex' }} >
-            <div style={{ width: 350, height: 600, margin: '80px 53px 0 80px', background: '#D9D9D9', borderRadius: 24 }} >
-                <div style={{ left: 220, top: 160, position: 'absolute', color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word' }}>filters</div>
+            <div style={{
+                width: 350,
+                height: 41,
+                margin: '80px 53px 0 80px',
+                background: '#D9D9D9',
+                borderRadius: '24px 24px 0 0',
+                position: 'relative', /* Set position relative */
+            }}>
+                <div style={{
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)', /* Center the text */
+                    color: 'black',
+                    fontSize: 24,
+                    fontFamily: 'Inter',
+                    fontWeight: '400',
+                    wordWrap: 'break-word'
+                }}>
+                    filters
+                </div>
+                <div style={{
+                    width: 350,
+                    margin: '40px 53px 0 0',
+                    background: '#D9D9D9',
+                    borderRadius: '0 0 24px 24px',
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    padding: '20px',
+                }}>
+                    {filters.map((role, idx) => (
+                        <span
+                            className={`filter ${clickedFilters[role] ? "active" : ""}`}
+                            style={{
+                                background: clickedFilters[role] ? 'blue' : 'rgb(247 244 244)',
+                                color: clickedFilters[role] ? 'white' : 'black'
+                            }}
+                            key={`filters-${idx}`}
+                            onClick={() => handleClick(role)}>
+                            {role}
+                        </span>
+                    ))}
+
+                </div>
             </div>
             <div className='container' style={{ marginInline: 50 }}>
                 <h4 className='my-4'>Showing results number</h4>
                 <div className="row">
-                    {posts.map((post) => {
-                        return <div className="col-md-12" key={post._id} >
+                    {filteredPosts.map((post) => {
+                        return <div className="col-md-12" key={`posts-${post._id}`} >
                             <PostCard post={post} />
                         </div>
                     })}
                 </div>
             </div>
         </div>
-        // <div style={{ marginLeft: 205, height: '100%', top:-100, position: 'relative', background: 'white' }}>   
-        //     <div style={{ left: 129, top: 271, position: 'absolute', color: 'black', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word' }}>Suggest similar jobs with toggle</div>
-        //     <div style={{ left: 220, top: 190, position: 'absolute', color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word' }}>filters</div>
-        //     <div style={{ left: 415, top: 154, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word' }}>Showing results number</div>
-        //     <div style={{ width: 287, height: 38, left: 758, top: 43, position: 'absolute' }}>
-        //         <div style={{ width: 28, height: 28, left: 247, top: 5, position: 'absolute' }}>
-        //         </div>
-        //     </div>
-
-        //     
-
-        // </div>
     )
 }
 
--- src/Components/PostCard.js
+++ src/Components/PostCard.js
@@ -33,11 +33,9 @@ const PostCard = (props) => {
                             <div style={{ width: 12, height: 18, position: 'absolute', border: '1.50px black solid' }}></div>
                         </div>
                         <div style={{ flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 20, display: 'inline-flex' }}>
-                            {/* <div style={{display: 'flex' }}>    */}
                             <div style={{ justifyContent: 'flex-start', alignItems: 'flex-start', gap: 559, display: 'inline-flex', width: 600 }}>
                                 <div style={{ color: 'black', fontSize: 32, fontFamily: 'Montserrat', fontWeight: '600', wordWrap: 'break-word' }}>{post.role}</div>
                             </div>
-                            {/* </div>  */}
                             <div style={{ color: 'black', fontSize: 20, fontFamily: 'Montserrat', fontWeight: '400', wordWrap: 'break-word' }}>{post.companyName}</div>
                             <div style={{ display: 'flex' }}>
                                 <div style={{ gap: 10, display: 'inline-flex', flexDirection: 'column', marginRight: 150 }}>
@@ -73,84 +71,13 @@ const PostCard = (props) => {
                         </div>
                     </div>
                     <div style={{ justifyContent: 'flex-start', alignItems: 'flex-end', gap: 573, display: 'inline-flex' }}>
-                    <div style={{color: '#555555', fontSize: 14, fontFamily: 'Montserrat', fontWeight: '400', wordWrap: 'break-word'}}>{displayString}</div>
+                        <div style={{ color: '#555555', fontSize: 14, fontFamily: 'Montserrat', fontWeight: '400', wordWrap: 'break-word' }}>{displayString}</div>
                         <div style={{ paddingLeft: 41, paddingRight: 41, paddingTop: 15, paddingBottom: 15, background: '#2539F0', borderRadius: 8, overflow: 'hidden', justifyContent: 'center', alignItems: 'center', gap: 10, display: 'flex' }}>
                             <div style={{ color: 'white', fontSize: 20, fontFamily: 'Montserrat', fontWeight: '600', wordWrap: 'break-word' }}>Apply now</div>
                         </div>
                     </div>
                 </div>
             </div>
-            {/* <div style={{ 
-                marginTop: '10px', 
-                display: 'flex', 
-                justifyContent: 'space-between', 
-                alignItems: 'center' 
-            }}>
-                <div style={{ 
-                    fontSize: '0.8rem', 
-                    color: '#555555' 
-                }}>Posted 3 days ago</div>
-                <button style={{ 
-                    padding: '8px 16px', 
-                    background: '#2539F0', 
-                    color: 'white', 
-                    borderRadius: '4px', 
-                    border: 'none' 
-                }}>Apply now</button>
-            </div> */}
-            {/* <div style={{ width: 925, paddingLeft: 24, paddingRight: 24, paddingTop: 20, paddingBottom: 20, position: 'absolute', background: 'white', borderRadius: 24, overflow: 'hidden', border: '1px rgba(0, 0, 0, 0.50) solid', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 10, display: 'inline-flex' }}>
-        <div style={{ flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 11, display: 'flex' }}>
-            <div style={{ justifyContent: 'flex-start', alignItems: 'flex-start', gap: 24, display: 'inline-flex' }}>
-                <div style={{ width: 61, height: 61, background: '#D9D9D9', borderRadius: 9999 }} />
-                <div style={{ flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 20, display: 'inline-flex' }}>
-                    <div style={{ justifyContent: 'flex-start', alignItems: 'flex-start', gap: 559, display: 'inline-flex' }}>
-                        <div style={{ color: 'black', fontSize: 32, fontFamily: 'Montserrat', fontWeight: '600', wordWrap: 'break-word' }}>{post.role}</div>
-                        <div style={{ width: 24, height: 24, position: 'relative' }}>
-                            <div style={{ width: 12, height: 18, left: 6, top: 3, position: 'absolute', border: '1.50px black solid' }}></div>
-                        </div>
-                    </div>
-                    <div style={{ color: 'black', fontSize: 20, fontFamily: 'Montserrat', fontWeight: '400', wordWrap: 'break-word' }}>{post.companyName}</div>
-                    <div style={{ justifyContent: 'flex-start', alignItems: 'flex-start', gap: 94, display: 'inline-flex' }}>
-                        <div style={{ justifyContent: 'flex-start', alignItems: 'center', gap: 8, display: 'flex' }}>
-                            <div style={{ width: 24, height: 24, position: 'relative' }}>
-                                <div style={{ width: 14.19, height: 18.02, left: 4.90, top: 3, position: 'absolute', background: '#555555' }}></div>
-                            </div>
-                            <div style={{ color: '#555555', fontSize: 16, fontFamily: 'Montserrat', fontWeight: '400', wordWrap: 'break-word' }}>Delhi</div>
-                        </div>
-                        <div style={{ justifyContent: 'flex-start', alignItems: 'center', gap: 8, display: 'flex' }}>
-                            <div style={{ width: 24, height: 24, position: 'relative' }}>
-                                <div style={{ width: 20, height: 20, left: 2, top: 2, position: 'absolute', background: '#555555' }}></div>
-                            </div>
-                            <div style={{ color: '#555555', fontSize: 16, fontFamily: 'Montserrat', fontWeight: '400', wordWrap: 'break-word' }}>Duration</div>
-                        </div>
-                    </div>
-                    <div style={{ justifyContent: 'flex-start', alignItems: 'flex-start', gap: 78, display: 'inline-flex' }}>
-                        <div style={{ justifyContent: 'flex-start', alignItems: 'center', gap: 8, display: 'flex' }}>
-                            <div style={{ width: 24, height: 24, position: 'relative' }}>
-                                <div style={{ width: 7, height: 7, left: 8.50, top: 12.50, position: 'absolute', background: '#555555' }}></div>
-                                <div style={{ width: 21, height: 21.34, left: 1.50, top: 0.66, position: 'absolute', background: '#555555' }}></div>
-                            </div>
-                            <div style={{ color: '#555555', fontSize: 16, fontFamily: 'Montserrat', fontWeight: '400', wordWrap: 'break-word' }}>Unpaid</div>
-                        </div>
-                        <div style={{ justifyContent: 'flex-start', alignItems: 'flex-start', gap: 10, display: 'flex' }}>
-                            <div style={{ width: 20, height: 20, position: 'relative' }}>
-                                <div style={{ width: 17, height: 13.50, left: 1.50, top: 3.50, position: 'absolute', background: '#555555' }}></div>
-                            </div>
-                            <div style={{ color: '#555555', fontSize: 16, fontFamily: 'Montserrat', fontWeight: '400', wordWrap: 'break-word' }}>1233 applicants</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div style={{ justifyContent: 'flex-start', alignItems: 'flex-end', gap: 573, display: 'inline-flex' }}>
-                <div style={{ color: '#555555', fontSize: 14, fontFamily: 'Montserrat', fontWeight: '400', wordWrap: 'break-word' }}>Posted 3 days ago</div>
-                <div style={{ paddingLeft: 41, paddingRight: 41, paddingTop: 15, paddingBottom: 15, background: '#2539F0', borderRadius: 8, overflow: 'hidden', justifyContent: 'center', alignItems: 'center', gap: 10, display: 'flex' }}>
-                    <div style={{ color: 'white', fontSize: 20, fontFamily: 'Montserrat', fontWeight: '600', wordWrap: 'break-word' }}>Apply now</div>
-                </div>
-            </div>
-        </div>
-        </div>  */}
-
-
         </div>
     )
 }
